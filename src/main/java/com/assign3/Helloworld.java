9afe51a7f8ec5c02875fd9a68932a85ecd60a566
API1190 <suyash.gautam@apisero.com>
2021-06-11 23:52:24 +0530

    Project files

---
+++ src/main/java/com/assign3/Helloworld.java
@@ -0,0 +1,12 @@
+package com.assign3;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Line2");
+		System.out.println("Line3");
+	}
+
+}
